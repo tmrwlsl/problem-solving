bcdbc2c422b61feffdfbff7ec7719b452fd3f643
Seokjin Lee <46406119+tmrwlsl@users.noreply.github.com>
2024-10-23 05:51:30 +0900

    feat: solve baekjoon problem #4562. No Brainer

---
+++ baekjoon/Baekjoon4562.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n ;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if(x >= y){
+                sb.append("MMM BRAINS\n");
+            }else{
+                sb.append("NO BRAINS\n");
+            }
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
