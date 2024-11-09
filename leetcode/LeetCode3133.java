47448b8ca071b467e9db80ee8507a156d1b9e529
Seokjin Lee <46406119+tmrwlsl@users.noreply.github.com>
2024-11-10 04:26:40 +0900

    feat: solve leetcode problem #3133. Minimum Array End

---
+++ leetcode/LeetCode3133.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long minEnd(int n, int x) {
+        long result = x;
+        n--;
+        for(long mask = 1; n > 0; mask <<= 1){
+            if((mask & x) == 0){
+                result |= (n & 1) * mask;
+                n >>= 1;
+            }
+        }
+        return result;
+    }
+}
